1155799f40ddf6fe860052a3ad0d79727075c149
ITMedmatch <it@medmatch.health>
2021-11-14 10:46:34 -0500

    07 - nav

--- cheffy/src/app/core.cljs
+++ cheffy/src/app/core.cljs
@@ -2,22 +2,23 @@
   (:require [reagent.core :as r]
             [re-frame.core :as rf]
             [app.db]
+            [app.nav.views.nav :refer [nav]]
             [app.theme :refer [cheffy-theme]]
             ["@smooth-ui/core-sc" :refer [Normalize ThemeProvider Button]]))
 
 (defn app
-  []
-     [:<>
-      [:> Normalize]
-      [:> ThemeProvider {:theme cheffy-theme}
-        [:> Button "Cheffy"]]])
+      []
+      [:<>
+       [:> Normalize]
+       [:> ThemeProvider {:theme cheffy-theme}
+        [nav]]])
 
 (defn ^:dev/after-load start
-  []
-  (rf/dispatch-sync [:initialize-db])
-  (r/render [app]
-    (.getElementById js/document "app")))
+      []
+      (rf/dispatch-sync [:initialize-db])
+      (r/render [app]
+                (.getElementById js/document "app")))
 
 (defn ^:export init
-  []
-  (start))
+      []
+      (start))
