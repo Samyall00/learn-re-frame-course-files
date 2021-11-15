2085049c2e3b3741d86e254260550636587cfd9f
ITMedmatch <it@medmatch.health>
2021-11-15 15:23:53 -0500

    17 - sign-up

--- cheffy/src/app/auth/events.cljs
+++ cheffy/src/app/auth/events.cljs
@@ -2,6 +2,22 @@
   (:require [re-frame.core :refer [reg-event-fx]]))
 
 (reg-event-fx
+  :sign-up ;; cofx {:db db :dispatch [:set-active-nav :saved]}
+  (fn [{:keys [db]} [_ {:keys [first-name last-name email password]}]]
+      {:db (-> db
+               (assoc-in [:auth :uid] email)
+               (assoc-in [:users email] {:id email
+                                         :profile {:first-name first-name
+                                                   :last-name last-name
+                                                   :email email
+                                                   :password password
+                                                   :img "img/avatar.jpg"}
+                                         :saved #{}
+                                         :inboxes {}}))
+       :dispatch [:set-active-nav :saved]}))
+
+
+(reg-event-fx
   :log-in ;; cofx {:db db :dispatch [:set-active-nav :saved]}
   (fn [{:keys [db]} [_ {:keys [email password]}]]
       (let [user (get-in db [:users email])
