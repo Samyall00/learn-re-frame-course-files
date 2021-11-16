214e584d841aca6b23cd57b67d880ebeb14951fc
ITMedmatch <it@medmatch.health>
2021-11-16 10:14:14 -0500

    18 - local-storage

--- cheffy/src/app/auth/subs.cljs
+++ cheffy/src/app/auth/subs.cljs
@@ -4,4 +4,10 @@
 (reg-sub
   :logged-in?
   (fn [db _]
-      (boolean (get-in db [:auth :uid]))))
+      (boolean (get-in db [:aut :uid]))))
+
+(reg-sub
+  :active-user-profile
+  (fn [db _]
+      (let [uid (get-in db [:auth :uid])]
+           (get-in db [:users uid :profile]))))
